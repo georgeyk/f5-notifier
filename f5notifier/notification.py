4390a11f03da237b4854e26dc17d479fff90fa96
georgeyk <georgeyk.dev@gmail.com>
2012-07-02 21:53:14 -0300

    refactor notification module.

--- f5notifier/notification.py
+++ f5notifier/notification.py
@@ -19,33 +19,11 @@
 ##
 ##
 
-import pynotify
+from gi.repository import Notify
 
 
-class Notificator(object):
-    def __init__(self):
-        pynotify.init('F5 Notifier')
-
-    def send_message(self, title, message):
-        note = pynotify.Notification(title, message)
-        note.set_urgency(pynotify.URGENCY_NORMAL)
-        note.show()
-
-    def send_message_with_callback(self, title, message, button_label,
-                                   callback):
-        note = pynotify.Notification(title, message)
-        note.set_urgency(pynotify.URGENCY_NORMAL)
-        note.add_action('clicked', button_label, callback, None)
-        note.show()
-
-
-def _on_click_button(*args, **kwargs):
-    print args, kwargs
-    print 'clicked'
-
-
-if __name__ == '__main__':
-    notifier = Notificator()
-    notifier.send_message('test', '123 foobar')
-    notifier.send_message_with_callback('test', 'with callback', 'Click me',
-                                        _on_click_button)
+def send_message(title, message):
+    Notify.init('F5 Notifer')
+    notification = Notify.Notification.new(title, message,
+                                           'dialog-information')
+    notification.show()
